6172d4f3e4627da672b688f186cafa1653833d35
Alexis Kotzambasis <lexiko80@gmail.com>
2019-12-15 00:01:47 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+var forLoop = (stringArray) =>{
+  for(let i = 0; i < 25; i++) {
+    if (i == 1) {
+      stringArray.push('I am 1 strange loop.')
+    else {
+      stringArray.push('I am ${i} strange loops.')
+    }
+    }
+  }
+}
